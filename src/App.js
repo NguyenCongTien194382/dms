21d265bbb1ef0b8a5d78cc0dd3b945e29844b332
NguyenCongTien194382 <tien.nc194382@sis.hust.edu.vn>
2024-03-15 11:50:51 +0700

    'firt'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Login from "./pages/Login/Login";
+import Navbar from "./components/Navbar/Navbar"
+import Header from './components/Header/Header'
+import ListDevice from "./components/ListDevice/ListDevice";
+import Login from "./components/Login/Login";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Login />
+    // <div className="main">
+    //   <Navbar />
+    //   <div className="content">
+    //     <Header />
+    //     <ListDevice />
+    //   </div>
+    // </div>
   );
 }
 
